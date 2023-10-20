b996b29e59bbe2ca0668f1358b2b4f30b6d6c7fc
evgen <evgeniy@gorod-lombard.ru>
2023-10-20 16:33:45 +0500

    first commit

---
+++ src/main/java/com/github/Evgenn91/dto/AdressDto.java
@@ -0,0 +1,16 @@
+package com.github.Evgenn91.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class AdressDto {
+    private String address;
+    private String lombardName;
+
+}
---
+++ src/main/java/com/github/Evgenn91/dto/PledgeDto.java
@@ -0,0 +1,8 @@
+package com.github.Evgenn91.dto;
+
+public class PledgeDto {
+    private String guid;
+    private String globalGuid;
+    private String loan;
+    private String debt;
+}
