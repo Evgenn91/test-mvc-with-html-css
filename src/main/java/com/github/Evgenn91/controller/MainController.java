d719a83e4824d585f93d5f7bfd9bf2432326d8b1
evgen <evgeniy@gorod-lombard.ru>
2023-10-26 12:46:38 +0500

    refactoring

--- src/main/java/com/github/Evgenn91/controller/MainController.java
+++ src/main/java/com/github/Evgenn91/controller/MainController.java
@@ -1,20 +1,22 @@
 package com.github.Evgenn91.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String firstPage() {
-//        model.addAttribute("name", name);
+    public String firstPage(Model model) {
+        model.addAttribute("name", "Тестовое имя");
         return "index";
     }
 
 
     @GetMapping("/test")
-    public String greeting() {
+    public String greeting(Model model) {
 //        model.addAttribute("name", name);
         return "test";
     }
